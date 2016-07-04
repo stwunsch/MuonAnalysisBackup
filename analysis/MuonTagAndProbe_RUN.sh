c9efb0092c02901caf7cc7a62863d1d5c5908434
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2016-07-04 17:56:06 +0200

    blub

--- analysis/MuonTagAndProbe_RUN.sh
+++ analysis/MuonTagAndProbe_RUN.sh
@@ -1,12 +1,43 @@
 #!/bin/bash
 
-# Set filepath for MC and DATA files here
-filename_MC="../../treeGeneration/tnpZ_MC.root"
-filename_DATA="../../../data/TnPTree_80X_Run2016B_v2_GoldenJSON_Run273731_to_274240_IncludingMissingLumi_Completed.root"
+# Setup
 
-# Perform pile-up reweighting according to number of primary vertices
-root -b -l -q $filename_MC $filename_DATA addNVtxWeight.cxx
+filename_DATA="../../data/TnPTree_80X_Run2016B_v2_GoldenJSON_Run273731_to_274240_IncludingMissingLumi_Completed.root ../../data/TnPTree_80X_Run2016B_v2_GoldenJSON_Run274241to274421.root ../../data/TnPTree_80X_Run2016B_v2_GoldenJSON_Run274422to274443.root ../../data/TnPTree_80X_Run2016B_v2_GoldenJSON_Run274444to275125.root"
+echo "[INFO] Input files:" $filename_DATA
 
-# Run efficiency measurement using tag and probe for MC and DATA
-cmsRun MuonTagAndProbe_DATA.py $filename_DATA
-cmsRun MuonTagAndProbe_MC.py tnpZ_withNVtxWeights.root
+filename_SUBTREE="subTree_Mu50.root"
+
+# Creating subtree if the file doesn't exist, otherwise skip producing subtree
+
+if [ -f $filename_SUBTREE ];
+then
+    echo "[INFO] File" $filename_SUBTREE "already exists, skip creating subtree."
+else
+    echo "[INFO] Creating subtree from input files."
+    root -l -b -q $filename_DATA subTree.C
+fi
+
+# Make high resolution eta plot
+
+if [ -f highResEta/MuonTagAndProbe_DATA.root ];
+then
+    echo "[INFO] Skip highResEta because ROOT file already exists"
+else
+    echo "[INFO] Start highResEta ..."
+    cd highResEta/
+    #cmsRun MuonTagAndProbe_DATA.py ../$filename_SUBTREE
+    cd ..
+fi
+
+# Make eta forward/backward plots
+
+if [ -f eta_1p2_1p7/MuonTagAndProbe_DATA_forward.root ];
+then
+    echo "[INFO] Skip eta_1p2_1p7 because ROOT file already exists"
+else
+    echo "[INFO] Start eta_1p2_1p7 ..."
+    cd eta_1p2_1p7/
+    cmsRun MuonTagAndProbe_DATA_forward.py ../$filename_SUBTREE
+    cmsRun MuonTagAndProbe_DATA_backward.py ../$filename_SUBTREE
+    cd ..
+fi
---
+++ analysis/subTree.C
@@ -0,0 +1,11 @@
+void subTree(TString dir="tpTree", TString cut="tag_IsoMu20", TString newFile="subTree_Mu50.root"){
+    TTree *in  = (TTree *)gFile->Get(dir+"/fitter_tree");
+    TFile *fout = new TFile(newFile, "RECREATE");
+    TDirectory *dout = fout->mkdir(dir); dout->cd();
+
+    TTree *out = in->CopyTree(cut);
+    std::cout << "INPUT TREE (" << in->GetEntries() << " ENTRIES)" << std::endl;
+    std::cout << "OUTPUT TREE (" << out->GetEntries() << " ENTRIES)" << std::endl;
+    dout->WriteTObject(out, "fitter_tree");
+    fout->Close();
+}
